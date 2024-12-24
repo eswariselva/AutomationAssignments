321e78bbc53e21382a186e34c4bb7f71b68bbbf4
eswariselva <eswariselva91@gmail.com>
2024-12-25 02:24:48 +0530

    Assignment 1 - Data types

---
+++ src/com/creatio/crm/language/basics/Assignment1Datatypes.java
@@ -0,0 +1,117 @@
+package com.creatio.crm.language.basics;
+
+public class Assignment1Datatypes {
+
+	public static void main(String[] args) {
+	
+		// Float datatype can store upto 6 /7 decimal digits
+		
+		float temp=25.5f;
+		System.out.println(temp);
+		
+		//Boolean datatype stores values as either true or false
+		
+		boolean orderplaced=true;
+		boolean ordernotplaced=false;
+		System.out.println(orderplaced);
+		System.out.println(ordernotplaced);
+		
+		//int datatype stores -10 digits to +10 digits
+		
+		int phoneno=123456789;
+		System.out.println(phoneno);
+				
+		
+		// Float datatype can store upto 6 /7 decimal digits
+		
+		float amount=1000.50f;
+		System.out.println(amount);
+		
+		//String datatype stores collection of characters
+		
+		String email="john.doe@example.com";
+		System.out.println(email);
+		
+		// Float datatype can store upto 6 /7 decimal digits
+		
+		float latitude=37.7749f; 
+		float longitude=-122.4194f;
+		System.out.println(latitude);
+		System.out.println(longitude);
+		
+		//Boolean datatype stores values as either true or false
+		
+		boolean married=true;
+		boolean unmarried=false;
+		System.out.println(married);
+		System.out.println(unmarried);
+				
+		//String datatype stores collection of characters
+				
+		String occupation="Software Engineer";
+		System.out.println(occupation);
+		
+		//String datatype stores collection of characters
+		
+		String color="Blue";
+		System.out.println(color);
+		
+		//short datatype stores -32768 to +32768
+		
+		short year=2023;
+		System.out.println(year);
+		
+		//int datatype stores -10 digits to +10 digits
+		
+		int followers=1000000;
+		System.out.println(followers);
+		
+		// Float datatype can store upto 6 /7 decimal digits
+		
+		float rating =7.5f; 
+	    System.out.println(rating);
+		
+		//Char datatype can store single character (ASCII Value)
+	    
+	    char blood='A';
+	    System.out.println(blood);
+	    
+	   //String datatype stores collection of characters
+		
+	  	String book="To Kill a Mockingbird";
+	  	System.out.println(book);
+	  	
+	   //short datatype stores -32768 to +32768
+		
+	    short empcount=500;
+	  	System.out.println(empcount);
+	  	
+       //String datatype stores collection of characters
+		
+	  	String time="2:30PM";
+	  	System.out.println(time);
+	  	
+       //String datatype stores collection of characters
+		
+	  	String name="United States of America";
+	  	System.out.println(name);
+	  	
+       //String datatype stores collection of characters
+		
+	  	String eyecolor="Brown";
+	  	System.out.println(eyecolor);
+	  	
+       //String datatype stores collection of characters
+		
+	  	String birthplace="New York City";
+	  	System.out.println(birthplace);
+	  	
+	 // Float datatype can store upto 6 /7 decimal digits
+		
+	    float distance =200.5f; 
+	 	System.out.println(distance);
+	  	
+	  	
+	  }
+
+}
